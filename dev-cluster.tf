6e84209bdc5c13c33f75938726cbc523fee34436
zrasul099 <zrasul099@gmail.com>
2024-03-25 19:40:11 -0500

    first

--- dev-cluster.tf
+++ dev-cluster.tf
@@ -4,11 +4,11 @@ provider "google" {
 }
 
 module "qa_gke_cluster" {
-    source = "github.com/zrasul099/tf-module.git"
-    service_account_id           = "qa-gke-service-account"
-    service_account_display_name = "QA GKE Service Account"
-    cluster_name                 = "qa-cluster"
-    cluster_location             = "us-central1-b"
-    project_id                   = "proven-dryad-417822"
+  source                       = "github.com/zrasul099/tf-module.git"
+  service_account_id           = "qa-gke-service-account"
+  service_account_display_name = "QA GKE Service Account"
+  cluster_name                 = "qa-cluster"
+  cluster_location             = "us-central1-b"
+  project_id                   = "proven-dryad-417822"
 }
 ##
